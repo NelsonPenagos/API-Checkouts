523b00106a93bf07ae67b98fdc2055184609a5d6
Nelson Penagos <nelson.penagosj@gmail.com>
2020-06-27 18:57:42 -0500

    Initial commit

---
+++ checkout-config-server/src/test/java/com/checkout/configServer/CheckoutConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkout.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
